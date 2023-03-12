e92cf918db9cb8fd7b75601d6bcbbd8b8739e16f
EricBermudezHernandez <91663286+EricBermudezHernandez@users.noreply.github.com>
2023-03-12 22:05:00 +0000

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,38 +1,10 @@
 #include "big_int.hpp"
 #include "board.hpp"
 #include "bigint_exceptions.hpp"
+#include "number.hpp"
 
 int main(int argc, char* argv[]) {
-  std::string nombre_fichero(argv[1]), primera_linea;
-  std::ifstream fichero;
-  fichero.open(nombre_fichero.c_str(), std::ios::in);
-  fichero >> primera_linea;
-  fichero >> primera_linea;
-  fichero >> primera_linea;
-  int base{std::stoi(primera_linea)};
-  switch (base) {
-    case 2: {
-      Board<2> pizarra_base2{nombre_fichero};
-      pizarra_base2.Start();
-      break;
-    }
-    case 8: {
-      Board<8> pizarra_base8{nombre_fichero};
-      pizarra_base8.Start();
-      break;
-    }
-    case 10: {
-      Board<10> pizarra_base10{nombre_fichero};
-      pizarra_base10.Start();
-      break;
-    }
-    case 16: {
-      Board<16> pizarra_base16{nombre_fichero};
-      pizarra_base16.Start();
-      break;
-    }
-    default:
-      throw(BigIntBaseNotImplemented("La base " + std::to_string(base) + " no es compatible con el tipo de dato BigInt\n"));
-      break;
-  }
+  std::string nombre_fichero(argv[1]);
+  Board pizarra{nombre_fichero};
+  pizarra.Start();
 }
