423b0facd44d05e8528d662967b6d5a2e6087c74
EricBermudezHernandez <91663286+EricBermudezHernandez@users.noreply.github.com>
2023-03-12 14:51:24 +0000

    Update board.hpp

--- board.hpp
+++ board.hpp
@@ -13,8 +13,8 @@
 #include <vector>
 
 #include "big_int.hpp"
+#include "number.hpp"
 
-template <size_t Base>
 class Board {
  public:
   // Constructor
@@ -24,52 +24,53 @@ class Board {
   void Start();
 
  private:
-  std::map<std::string, BigInt<Base>> variables_;
+  std::map<std::string, Number*> variables_;
   std::ifstream fichero_;
 };
 
-template <size_t Base>
-Board<Base>::Board(const std::string& fichero) {
+
+Board::Board(const std::string& fichero) {
   fichero_.open(fichero.c_str(), std::ios::in);
 }
 
 // Método que empieza a leer todas las líneas del fichero y las procesa,
 // metiendo los valores y evaluando las expresiones en RPN
-template <size_t Base>
-void Board<Base>::Start() {
-  BigInt<Base> operando_1, operando_2,
-      resultado;  // Se definen los operandos para las operaciones y el
+
+void Board::Start() {
+  Number* operando_1{NULL}; 
+  Number* operando_2{NULL};  // Se definen los operandos para las operaciones y el
                   // resultado
-  std::stack<BigInt<Base>>
+  std::stack<Number*>
       pila;  // Se crea una pila para guardar los operandos y resultados
   std::ofstream
       archivo_salida;  // Este archivo no se usa, por lo que se puede eliminar
   std::string linea, palabra, identificador;  // Se definen variables para
                                               // procesar las líneas del fichero
+  size_t base; // variable que nos va a servir para guardar la base
   std::istringstream
       linea_stream;  // Se crea un flujo de entrada para procesar las líneas
-  std::getline(fichero_, linea);  // Se lee la primera línea del fichero y se
-                                  // guarda en la variable "linea"
   std::ofstream fichero_salida;   // Se crea un archivo de salida para guardar
                                   // los resultados
   fichero_salida.open("fichero_salida.txt",
                       std::ios::out);  // Se abre el archivo de salida
-  fichero_salida
-      << linea;  // Se escribe la primera línea en el archivo de salida
-  fichero_salida
-      << std::endl;  // Se escribe un salto de línea en el archivo de salida
-  while (
-      std::getline(fichero_, linea)) {  // Se itera sobre las líneas del fichero
-                                        // (omitiendo la primera línea)
+  while (!fichero_.eof()) {  // Se itera sobre las líneas del fichero
+    std::getline(fichero_, linea);
     linea_stream.str(linea);  // Se establece la línea actual como entrada para el flujo
     linea_stream >> palabra;  // Se lee la primera palabra de la línea
     identificador = palabra;  // Se guarda la primera palabra como identificador
                               // de la variable
-    linea_stream >> palabra;  // Se lee la siguiente palabra
+    linea_stream >> palabra;  // Se lee la siguiente palabra, esto sería el "igual" (=)
+    if (palabra == "=") { // Si encontramos un igual, es por que la línea será la declaración de un "Number*"
+      // Después de esto, va la base así que la guardamos 
+      linea_stream >> palabra;
+      // Eliminamos la coma (',') que está al final de la base ej: "2," y lo almacenamos en la variable "base" de tipo size_t
+      palabra.erase((palabra.size() - 1), 1);
+      base = std::stoul(palabra);
+    }
     while (linea_stream >> palabra) {  // Se itera sobre las palabras restantes de la línea
-      if (variables_.find(palabra) !=
-          variables_.end()) {  // Si la palabra es una variable ya definida, se
-                               // apila su valor en la pila
+      if (variables_.find(palabra) != variables_.end()) {  // Si la palabra es una variable ya definida, se
+                                                          // apila su valor en la pila
+        std::cout << variables_[palabra] << std::endl;
         pila.push(variables_[palabra]);
       } else if (palabra == "+") {  // Si la palabra es un operador de suma, se
                                     // sacan dos elementos de la pila, se suman,
@@ -78,38 +79,43 @@ void Board<Base>::Start() {
         pila.pop();
         operando_1 = pila.top();
         pila.pop();
-        resultado = operando_1 + operando_2;
+        Number* resultado = Number::create(operando_1->GetBase() , "0");  
+        resultado = operando_1->add(operando_2);
         pila.push(resultado);
       } else if (palabra == "-") {  // Lo mismo para los otros operadores aritméticos
         operando_2 = pila.top();
         pila.pop();
         operando_1 = pila.top();
         pila.pop();
-        resultado = operando_1 - operando_2;
+        Number* resultado = Number::create(operando_1->GetBase() , "0");
+        resultado = operando_1->subtract(operando_2);
         pila.push(resultado);
       } else if (palabra == "*") {
         operando_2 = pila.top();
         pila.pop();
         operando_1 = pila.top();
         pila.pop();
-        resultado = operando_1 * operando_2;
+        Number* resultado = Number::create(operando_1->GetBase() , "0");
+        resultado = operando_1->multiply(operando_2);
         pila.push(resultado);
       } else if (palabra == "/") {
         operando_2 = pila.top();
         pila.pop();
         operando_1 = pila.top();
         pila.pop();
-        resultado = operando_1 / operando_2;
+        Number* resultado = Number::create(operando_1->GetBase() , "0");
+        resultado = operando_1->divide(operando_2);
         pila.push(resultado);
       } else if (palabra == "%") {
         operando_2 = pila.top();
         pila.pop();
         operando_1 = pila.top();
         pila.pop();
-        resultado = operando_1 % operando_2;
+        Number* resultado = Number::create(operando_1->GetBase() , "0");
+        resultado = operando_1->module(operando_2);
         pila.push(resultado);
       } else {
-        BigInt<Base> numero(palabra);
+        Number* numero = Number::create(base, palabra);
         pila.push(numero);
       }
     }
